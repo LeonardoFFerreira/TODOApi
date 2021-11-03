f10282b3e3fc9933e62856d68b454de11580f2c7
LeonardoFFerreira <devleofreitas@gmail.com>
2021-11-03 10:11:37 -0300

    Criando UpdateTodoCommand e MarkTodoAsUndoneCommand

---
+++ Todo.Domain/Commands/UpdateTodoCommand.cs
@@ -0,0 +1,34 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    class UpdateTodoCommand : Notifiable, ICommand
+    {
+        public UpdateTodoCommand()
+        {
+        }
+        public UpdateTodoCommand(Guid id, string title, string user)
+        {
+            Id = id;
+            Title = title;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasMinLen(Title, 3, "Title", "O título deve conter mais de 3 caracteres")
+                .HasMinLen(User, 6, "User", "Usuário inválido")
+                );
+        }
+    }
+}
