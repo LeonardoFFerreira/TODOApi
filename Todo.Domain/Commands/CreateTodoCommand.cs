bf2b17a3e63f56f987b25590d30d95e4966b8935
LeonardoFFerreira <devleofreitas@gmail.com>
2021-11-03 00:11:57 -0300

    Definindo contrato do CreateTodoCommmand

---
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,32 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateTodoCommand : Notifiable, ICommand
+    {
+        public string Title { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public CreateTodoCommand() { }
+
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasMinLen(Title, 3, "Title", "O título deve conter mais de 3 caracteres")
+                .HasMinLen(User, 3, "User", "Usuário deve conter mais de 3 caracteres")
+                );
+        }
+    }
+}
