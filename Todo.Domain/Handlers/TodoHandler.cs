7555c8b22f477b77f0b42c8ea535d12312f696eb
LeonardoFFerreira <devleofreitas@gmail.com>
2021-11-03 21:21:48 -0300

    Criando camada Handler e o TodoHandler com o método CreateTodoCommand

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,33 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            //Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "A tarefa possui campos inválidos", command.Notifications);
+
+            TodoItem todo = new TodoItem(command.Title, command.Date, command.User);
+
+            _repository.Create(todo);
+
+            return new GenericCommandResult(true, "Tarefa salva com sucesso", todo);
+        }
+    }
+}
