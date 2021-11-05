4522e6dd54990cec3463b7c3081fb2587a5c952c
LeonardoFFerreira <devleofreitas@gmail.com>
2021-11-05 10:08:34 -0300

    Implementado teste na camada Entity/TodoItem - Red Green Refectory

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly TodoItem _validTodoItem = new TodoItem("Novo todo", DateTime.Now, "Leonardo");
+
+        [TestMethod]
+        public void Dado_um_novo_todo_o_mesmo_nao_pode_estar_concuido()
+        {
+            Assert.AreEqual(_validTodoItem.Done, false);
+        }
+
+    }
+}
