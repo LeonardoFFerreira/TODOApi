55822bd76f88d272c6aa81d5f7f4210ac2727c49
LeonardoFFerreira <devleofreitas@gmail.com>
2021-12-23 21:01:25 -0300

    resolvendo dependencias

---
+++ Todo.Domain.Tests/QueryTests/TodoQueryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Todo.Domain.Entities;
+using Todo.Domain.Queries;
+
+namespace Todo.Domain.Tests.QueryTests
+{
+    [TestClass]
+    public class TodoQueryTests
+    {
+        private List<TodoItem> _todoItems;
+
+        public TodoQueryTests()
+        {
+            _todoItems = new List<TodoItem>();
+
+            _todoItems.Add(new TodoItem("Tarefa 1", DateTime.Now, "user A"));
+            _todoItems.Add(new TodoItem("Tarefa 2", DateTime.Now, "user A"));
+            _todoItems.Add(new TodoItem("Tarefa 3", DateTime.Now, "leonardoferreira"));
+            _todoItems.Add(new TodoItem("Tarefa 4", DateTime.Now, "user A"));
+            _todoItems.Add(new TodoItem("Tarefa 5", DateTime.Now, "user A"));
+
+        }
+
+
+        [TestMethod]
+        public void Deve_retornar_tarefas_somente_do_usuario_indicado()
+        {
+            var result = _todoItems.AsQueryable().Where(TodoQueries.GetAll("leonardoferreira"));
+
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
---
+++ Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries
+{
+    public static class TodoQueries
+    {
+        public static Expression<Func<TodoItem, bool>> GetAll(string user)
+        {
+            return todos => todos.User == user;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetAllDone(string user)
+        {
+            return todos => todos.User == user && todos.Done == true;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetAllUndone(string user)
+        {
+            return todos => todos.User == user && todos.Done == false;
+        }
+
+        public static Expression<Func<TodoItem, bool>> GetByDate(string user, DateTime date, bool done)
+        {
+            return todos => todos.User == user &&
+                            todos.Date == date.Date &&
+                            todos.Done == done;
+        }
+
+    }
+}
